5723019b349f0482ea2dad1126d2060ac56b4ce7
David William <sluuckejoohn@gmail.com>
2026-06-20 21:20:42 -0300

    feat: estrutura base preprocessador

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -9,3 +9,20 @@ def preprocess_text(text) -> str:
     # 3. Remoção de stop words
     # 4. Stemming
     pass
+
+def split_sentences(text: str) -> list[str]:
+    # implementar função para dividir o texto em frases, utilizando regex ou biblioteca de NLP
+    pass
+
+
+def tokenize(sentence: str) -> list[str]:
+    # implementar função para tokenizar uma frase em palavras, utilizando regex ou biblioteca de NLP
+    pass
+
+def remove_stopwords(tokens: list[str]) -> list[str]:
+    # implementar função para remover stop words de uma lista de tokens, utilizando uma lista pré-definida de stop words usar lib spacy
+    pass
+
+def stem(word: str) -> str:
+    # implementar função para aplicar stemming a uma palavra
+    pass
